b2b9c62ef84cde6ae5de8be0ff6840083b560790
Vincent Schlosser <s.vincent@live.fr>
2021-05-10 14:18:52 +0200

    Get all producers

--- controllers/ProducerController.js
+++ controllers/ProducerController.js
@@ -1,4 +1,10 @@
+const Producer = require('../models').Producer;
+
 class ProducerController {
+
+  async getAll() {
+    return Producer.findAll();
+  }
 
 }
 
--- routes/producers.js
+++ routes/producers.js
@@ -1,8 +1,12 @@
+const ProducerController = require('../controllers').ProducerController;
+
 let express = require('express');
 let router = express.Router();
 
 router.get('/producers', async (req, res, next) => {
-  res.json({hello: 'world'});
+  res.json(await ProducerController.getAll());
 });
+
+
 
 module.exports = router;
