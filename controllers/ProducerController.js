b40e9bbd4c51452704565f5875ad0be782a7a860
Vincent Schlosser <s.vincent@live.fr>
2021-05-10 16:16:25 +0200

    Finish crud producer

--- controllers/ProducerController.js
+++ controllers/ProducerController.js
@@ -6,6 +6,30 @@ class ProducerController {
     return Producer.findAll();
   }
 
+  async getById(id) {
+    return Producer.findByPk(id);
+  }
+
+  async add(data) {
+    return Producer.create(data)
+  }
+
+  async update(id, payload) {
+    return Producer.update(payload, {
+      where: {
+        id: id
+      }
+    });
+  }
+
+  async delete(id) {
+    return Producer.destroy({
+      where: {
+        id: id
+      }
+    })
+  }
+
 }
 
 module.exports = new ProducerController();
